0220aa5a2d11b7484b4aa464024ba8b6942d646f
Max Lv <max.c.lv@gmail.com>
2017-02-11 09:08:35 +0800

    Fix #1

--- include/libcork/config/linux.h
+++ include/libcork/config/linux.h
@@ -15,7 +15,11 @@
  * Endianness
  */
 
+#if defined(__FreeBSD_kernel__)
+#include <sys/endian.h>
+#else
 #include <endian.h>
+#endif
 
 #if __BYTE_ORDER == __BIG_ENDIAN
 #define CORK_CONFIG_IS_BIG_ENDIAN      1
